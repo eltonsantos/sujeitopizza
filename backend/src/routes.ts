7fe3621fbaf9755a9ab8446b1b5e5ce16f81bf5f
Elton Santos <eltonaxl@hotmail.com>
2023-05-20 23:35:19 -0300

    List by product by category

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -9,6 +9,7 @@ import { CreateCategoryController } from './controllers/category/CreateCategoryC
 import { ListCategoryController } from "./controllers/category/ListCategoryController"
 
 import { CreateProductController } from "./controllers/product/CreateProductController"
+import { ListByCategoryController } from "./controllers/product/ListByCategoryController"
 
 import { isAuthenticated } from "./middlewares/isAuthenticated"
 
@@ -21,15 +22,14 @@ const upload = multer(uploadConfig.upload("./tmp"))
 //-- ROTAS USER --
 router.post('/users', new CreateUserController().handle)
 router.post('/session', new AuthUserController().handle)
-
 router.get('/me', isAuthenticated , new DetailUserController().handle)
 
 //-- ROTAS CATEGORY
 router.post('/category', isAuthenticated, new CreateCategoryController().handle )
-
 router.get('/category', isAuthenticated, new ListCategoryController().handle )
 
 //-- ROTAS PRODUCT
 router.post('/product', isAuthenticated, upload.single("file"), new CreateProductController().handle )
+router.get('/category/product', isAuthenticated, new ListByCategoryController().handle )
 
 export { router }
