ccb1b230664149c904a6b99a5fc7b94af2202058
Elton Santos <eltonaxl@hotmail.com>
2023-05-20 21:10:41 -0300

    Login user

---
+++ backend/src/services/user/AuthUserService.ts
@@ -0,0 +1,33 @@
+import { compare } from "bcryptjs";
+import prismaClient from "../../prisma";
+
+interface AuthRequest {
+  email: string;
+  password: string;
+}
+
+class AuthUserService {
+  async execute({email, password}: AuthRequest) {
+    
+    const user = await prismaClient.user.findFirst({
+      where: {
+        email: email
+      }
+    })
+
+    if(!user){
+      throw new Error("User/Passaword incorrect")
+    }
+
+    const passwordMatch = await compare(password, user.password)
+
+    if(!passwordMatch){
+      throw new Error("User/Passaword incorrect")
+    }
+  
+
+    return { ok: true }
+  }
+}
+
+export { AuthUserService };
