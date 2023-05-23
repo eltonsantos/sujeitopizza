ad4da5fc9222b041d4a3ed1a761e61c2b22dfdad
Elton Santos <eltonaxl@hotmail.com>
2023-05-23 00:45:18 -0300

    Register user

--- frontend/src/contexts/AuthContext.tsx
+++ frontend/src/contexts/AuthContext.tsx
@@ -5,11 +5,13 @@ import { api } from '../services/apiClient';
 import { destroyCookie, setCookie, parseCookies } from 'nookies'
 import Router from 'next/router';
 
+
 type AuthContextData = {
   user: UserProps | undefined;
   isAuthenticated: boolean;
   signIn: (credentials: SignInProps) => Promise<void>;
   signOut: () => void;
+  signUp: (credentials: SignUpProps) => Promise<void>;
 }
 
 type UserProps = {
@@ -23,11 +25,18 @@ type SignInProps = {
   password: string;
 }
 
+type SignUpProps = {
+  name: string;
+  email: string;
+  password: string;
+}
+
 type AuthProviderProps = {
   children: ReactNode;
 }
 
 export const AuthContext = createContext({} as AuthContextData)
+
 
 export function signOut(){
   try{
@@ -69,13 +78,33 @@ export function AuthProvider({ children }: AuthProviderProps){
       //Redirecionar o user para /dashboard
       Router.push('/dashboard')
 
+
     }catch(err){
       console.log("ERRO AO ACESSAR ", err)
     }
   }
 
+
+  async function signUp({ name, email, password}: SignUpProps){
+    try{
+      
+      const response = await api.post('/users', {
+        name,
+        email,
+        password
+      })
+
+      console.log("CADASTRADO COM SUCESSO!")
+
+      Router.push('/')
+
+    }catch(err){
+      console.log("erro ao cadastrar ", err)
+    }
+  }
+
   return(
-    <AuthContext.Provider value={{ user, isAuthenticated, signIn, signOut }}>
+    <AuthContext.Provider value={{ user, isAuthenticated, signIn, signOut, signUp }}>
       {children}
     </AuthContext.Provider>
   )
--- frontend/src/pages/signup/index.tsx
+++ frontend/src/pages/signup/index.tsx
@@ -1,15 +1,49 @@
-import Head from 'next/head';
+import { useState, FormEvent, useContext  } from 'react'
+
+import Head from 'next/head'
 import Image from 'next/image';
 import styles from '../../../styles/home.module.scss';
 
 import logoImg from '../../../public/logo.svg';
 
-import { Button } from '../../components/ui/Button';
-import { Input } from '../../components/ui/Input';
+import { Input } from '../../components/ui/Input'
+import { Button } from '../../components/ui/Button'
+
+import { AuthContext } from '../../contexts/AuthContext'
 
 import Link from 'next/link';
 
 export default function SignUp() {
+  const { signUp } = useContext(AuthContext);
+
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const [loading, setLoading] = useState(false);
+
+  async function handleSignUp(event: FormEvent){
+    event.preventDefault();
+
+    if(name === '' || email === '' || password === ''){
+      alert("PREENCHA TODOS OS CAMPOS")
+      return;
+    }
+
+    setLoading(true);
+
+    let data = {
+      name,
+      email,
+      password
+    }
+
+    await signUp(data)
+
+    setLoading(false);
+
+  }
+
   return (
     <>
     <Head>
@@ -21,31 +55,37 @@ export default function SignUp() {
       <div className={styles.login}>
         <h1>Criando sua conta</h1>
 
-        <form>
+        <form onSubmit={handleSignUp}>
           <Input
             placeholder="Digite seu nome"
             type="text"
+            value={name}
+            onChange={ (e) => setName(e.target.value) }
           />
 
           <Input
             placeholder="Digite seu email"
             type="text"
+            value={email}
+            onChange={ (e) => setEmail(e.target.value) }
           />
 
           <Input
             placeholder="Sua senha"
             type="password"
+            value={password}
+            onChange={ (e) => setPassword(e.target.value) }
           />
           
           <Button
             type="submit"
-            loading={false}
+            loading={loading}
           >
             Cadastrar
           </Button>
         </form>
 
-        <Link href="/" legacyBehavior>
+        <Link href="/">
            <a className={styles.text}>Já possui uma conta? Faça login!</a>
         </Link>
 
