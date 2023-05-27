3d2ff08f1d5bac7fe80152b9f189f6162a11901a
Elton Santos <eltonaxl@hotmail.com>
2023-05-27 02:33:36 -0300

    Add product in DB and create validations

--- frontend/src/pages/product/index.tsx
+++ frontend/src/pages/product/index.tsx
@@ -1,4 +1,4 @@
-import { useState, ChangeEvent } from 'react'
+import { useState, ChangeEvent, FormEvent } from 'react'
 import Head from 'next/head';
 import styles from './styles.module.scss';
 import {Header} from '../../components/Header'
@@ -9,21 +9,26 @@ import { FiUpload } from 'react-icons/fi'
 
 import { setupAPIClient } from '../../services/api'
 
+import { toast } from 'react-toastify'
+
 type ItemProps = {
   id: string;
   name: string;
 }
 
-interface CategoryProps {
+interface CategoryProps{
   categoryList: ItemProps[];
 }
 
 export default function Product({ categoryList }: CategoryProps){
 
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState('');
+  const [description, setDescription] = useState('');
   const [avatarUrl, setAvatarUrl] = useState('');
   const [imageAvatar, setImageAvatar] = useState(null);
   const [categories, setCategories] = useState(categoryList || [])
-  const [categorySelected, setCategorySelected] = useState(0)
+  const [categorySelected, setCategorySelected] = useState('')
 
   function handleFile(e: ChangeEvent<HTMLInputElement>){
 
@@ -45,11 +50,48 @@ export default function Product({ categoryList }: CategoryProps){
   }
 
   //Quando você seleciona uma nova categoria na lista
-  function handleChangeCategory(event){
+  function handleChangeCategory(event: any){
     // console.log("POSICAO DA CATEGORIA SELECIONADA ", event.target.value)
     //console.log('Categoria selecionada ', categories[event.target.value])
 
     setCategorySelected(event.target.value)
+
+  }
+
+  async function handleRegister(event: FormEvent){
+    event.preventDefault();
+
+    try{
+      const data = new FormData();
+
+      if(name === '' || price === '' || description === '' || imageAvatar === null){
+        toast.error("Preencha todos os campos!");
+        return;
+      }
+
+      data.append('name', name);
+      data.append('price', price);
+      data.append('description', description);
+      data.append('category_id', categories[categorySelected].id);
+      data.append('file', imageAvatar);
+
+      const apiClient = setupAPIClient();
+
+      await apiClient.post('/product', data);
+
+      toast.success('Cadastrado com sucesso!')
+
+    }catch(err){
+      console.log(err);
+      toast.error("Ops erro ao cadastrar!")
+    }
+
+    setName('');
+    setPrice('');
+    setDescription('')
+    setImageAvatar(null);
+    setAvatarUrl('');
+    setCategorySelected('');
 
   }
 
@@ -64,7 +106,7 @@ export default function Product({ categoryList }: CategoryProps){
         <main className={styles.container}>
           <h1>Novo produto</h1>
 
-          <form className={styles.form}>
+          <form className={styles.form} onSubmit={handleRegister}>
 
             <label className={styles.labelAvatar}>
               <span>
@@ -74,18 +116,19 @@ export default function Product({ categoryList }: CategoryProps){
               <input type="file" accept="image/png, image/jpeg" onChange={handleFile} />
 
               {avatarUrl && (     
-                  <img 
-                    className={styles.preview}
-                    src={avatarUrl}
-                    alt="Foto do produto" 
-                    width={250}
-                    height={250}
-                  />
+                <img 
+                  className={styles.preview}
+                  src={avatarUrl}
+                  alt="Foto do produto" 
+                  width={250}
+                  height={250}
+                />
               )}
 
             </label>
 
             <select value={categorySelected} onChange={handleChangeCategory} >
+              <option value="">Selecione a categoria</option>
               {categories.map( (item, index) => {
                 return(
                   <option key={item.id} value={index}>
@@ -96,20 +139,26 @@ export default function Product({ categoryList }: CategoryProps){
             </select>
 
             <input 
-              type="text"
-              placeholder="Digite o nome do produto"
-              className={styles.input}
+            type="text"
+            placeholder="Digite o nome do produto"
+            className={styles.input}
+            value={name}
+            onChange={ (e) => setName(e.target.value) }
             />
 
             <input 
-              type="text"
-              placeholder="Preço do produto"
-              className={styles.input}
+            type="text"
+            placeholder="Preço do produto"
+            className={styles.input}
+            value={price}
+            onChange={ (e) => setPrice(e.target.value) }
             />      
 
             <textarea 
               placeholder="Descreva seu produto..."
               className={styles.input}
+              value={description}
+              onChange={ (e) => setDescription(e.target.value) }
             /> 
 
             <button className={styles.buttonAdd} type="submit">
