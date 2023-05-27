ba99ef9e75fe14570fab0888a049aea13d43bd1c
Elton Santos <eltonaxl@hotmail.com>
2023-05-27 03:02:34 -0300

    Configuring modal

--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -8,6 +8,10 @@ import { FiRefreshCcw } from 'react-icons/fi'
 
 import { setupAPIClient } from '../../services/api'
 
+import { ModalOrder } from '../../components/ModalOrder'
+
+import Modal from 'react-modal';
+
 type OrderProps = {
   id: string;
   table: string | number;
@@ -20,13 +24,53 @@ interface HomeProps{
   orders: OrderProps[];
 }
 
+export type OrderItemProps = {
+  id: string;
+  amount: number;
+  order_id: string;
+  product_id: string;
+  product:{
+    id: string;
+    name: string;
+    description: string;
+    price: string;
+    banner: string;
+  }
+  order:{
+    id: string;
+    table: string | number;
+    status: boolean;
+    name: string | null;
+  }
+}
+
 export default function Dashboard({ orders }: HomeProps){
 
   const [orderList, setOrderList] = useState(orders || [])
 
-  function handleOpenModalView(id: string){
-    alert("ID CLICADO " + id)
+  const [modalItem, setModalItem] = useState<OrderItemProps[]>()
+  const [modalVisible, setModalVisible] = useState(false);
+
+  function handleCloseModal(){
+    setModalVisible(false);
   }
+
+  async function handleOpenModalView(id: string){
+   
+     const apiClient = setupAPIClient(); 
+
+     const response = await apiClient.get('/order/detail', {
+       params:{
+        order_id: id,
+       } 
+     })
+
+     setModalItem(response.data);
+     setModalVisible(true);
+
+  }
+
+  Modal.setAppElement('#__next');
 
   return(
     <>
@@ -48,7 +92,7 @@ export default function Dashboard({ orders }: HomeProps){
         <article className={styles.listOreders}>
 
           {orderList.map( item => (
-            <section key={item.id} className={styles.orderItem}> 
+            <section  key={item.id} className={styles.orderItem}> 
               <button onClick={ () => handleOpenModalView(item.id) }>
                 <div className={styles.tag}></div>
                 <span>Mesa {item.table}</span>
@@ -59,6 +103,12 @@ export default function Dashboard({ orders }: HomeProps){
         </article>
 
       </main>
+
+      { modalVisible && (
+        <ModalOrder
+        
+        />
+      )}
 
     </div>
     </>
